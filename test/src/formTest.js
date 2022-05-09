937d2c4f5e44032fbf99037928b4b50bfe15ec14
Ravi Sawlani <ravisawlani04@gmail.com>
2022-05-10 01:11:26 +0530

    refactor

--- test/src/formTest.js
+++ test/src/formTest.js
@@ -2,21 +2,22 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot');
 expect.extend({ toMatchImageSnapshot });
 
 let component;  
+  
 beforeAll(async () => {
-    component = await browser.mountComponent('../../src/components/Form.jsx');
-  });
+  component = await browser.mountComponent('../../src/components/Form.jsx');
+});
 
-  afterAll(async () => {
-      await browser.quit();
-  });
+afterAll(async () => {
+    await browser.quit();
+});
 
-  it('should render functional components without error', async function() {
-    await browser.expect(component).to.be.visible;
-    const screenshot = await browser.screenshot();
-    expect(screenshot).toMatchImageSnapshot();
-  })
+it('should render functional components without error', async function() {
+  await browser.expect(component).to.be.visible;
+  const screenshot = await browser.screenshot();
+  expect(screenshot).toMatchImageSnapshot();
+})
 
-  it('test the dom snapshot', async function() {
-    const domSnapshot = await browser.getComponentDom();
-    expect(domSnapshot).toMatchSnapshot();
-  })
+it('test the dom snapshot', async function() {
+  const domSnapshot = await browser.getComponentDom();
+  expect(domSnapshot).toMatchSnapshot();
+})
